5a15d36f9bac4dca6be55c9666a27428ef734d33
vicchimenti <chimenti@seattleu.edu>
2019-10-11 11:01:20 -0700

    root contents

--- listdir_server.c
+++ listdir_server.c
@@ -18,6 +18,7 @@
 // #include <rpc/rpc.h>
 #include <stdio.h>
 #include <string.h>
+#include <errno.h>
 
 readdir_ret *
 readdir_1_svc(nametype *argp, struct svc_req *rqstp)
@@ -31,30 +32,35 @@ readdir_1_svc(nametype *argp, struct svc_req *rqstp)
 	printf("\nClient arg received: %s\n", *argp);
 
 	// open and assign directory
-	if ((dirp = opendir(".")) == NULL) {
-		perror ("Cannot open '.'");
-    exit (1);
-	}
-
-	// process argument passed from client
-	do {
-		errno = 0;
-		if ((dp = readdir(dirp)) != NULL) {
-			if (strcmp(dp->d_name, *argp) != 0) { continue; }
-				// troubleshooting print
-				(void) printf("\nDirectory Entry specified by client: %s\n", *argp);
-				(void) closedir(dirp);
-			}
-	} while (dp != NULL);
-
-	// process any errno
-	if (errno !=0) {
-		perror("Error reading directory");
-	} else {
+	if ((dirp = opendir("/")) == NULL) {
+		perror ("\nopendir() error\n");
 		(void) printf("Could not find client supplied directory: %s\n", *argp);
+	} else {
+		while((dp = readdir(dirp)) != NULL) {
+			printf("\nDirectory Entry specified by client: %s\n", dp->d_name);
+		}
+		closedir(dirp);
 	}
 
-	(void) closedir(dirp);
+	// // process argument passed from client
+	// do {
+	// 	errno = 0;
+	// 	if ((dp = readdir(dirp)) != NULL) {
+	// 		if (strcmp(dp->d_name, *argp) != 0) { continue; }
+	// 			// troubleshooting print
+	// 			(void) printf("\nDirectory Entry specified by client: %s\n", *argp);
+	// 			(void) closedir(dirp);
+	// 		}
+	// } while (dp != NULL);
+
+	// // process any errno
+	// if (errno !=0) {
+	// 	perror("Error reading directory");
+	// } else {
+	// 	(void) printf("Could not find client supplied directory: %s\n", *argp);
+	// }
+
+	// (void) closedir(dirp);
 
 	// return result is not yet assigned
 	return &result;
